7278e665d49fae63f5eb7f12f18a8059a38c1db3
liganggit <598010575@qq.com>
2017-02-27 21:35:38 +0800

    1.添加weiphp代码以及基础配置。

---
+++ Runtime/Temp/lgprogrammer_90c04c47a911bf44699edf5fd786dd52.php
@@ -0,0 +1,3 @@
+<?php
+//000000000000a:22:{s:2:"id";s:1:"1";s:3:"uid";s:1:"1";s:11:"public_name";s:18:"学习想法心得";s:9:"public_id";s:15:"gh_a22c6e849897";s:6:"wechat";s:12:"lgprogrammer";s:13:"interface_url";N;s:12:"headface_url";N;s:4:"area";N;s:12:"addon_config";N;s:12:"addon_status";N;s:5:"token";s:15:"gh_a22c6e849897";s:6:"is_use";s:1:"0";s:4:"type";s:1:"0";s:5:"appid";s:18:"wx122ad01fb326666a";s:6:"secret";s:32:"f2ad4514544dd95a021e202ac8541980";s:8:"group_id";s:1:"0";s:14:"encodingaeskey";s:43:"KZW1nWoMFg76saxQnVywlYt21aCz409D8IJyvrvlLpb";s:8:"tips_url";N;s:6:"domain";N;s:7:"is_bind";s:1:"0";s:5:"cTime";N;s:24:"authorizer_refresh_token";N;}
+?>
